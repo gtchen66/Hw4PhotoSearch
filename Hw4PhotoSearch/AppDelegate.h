bb10763c86f28bad6b350b4f3513e95e0426ac73
gtchen66 <gtchen66@yahoo.com>
2014-02-02 05:18:12 -0800

    Initial Commit

---
+++ Hw4PhotoSearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hw4PhotoSearch
+//
+//  Created by George Chen on 2/2/14.
+//  Copyright (c) 2014 George Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hw4PhotoSearchTests/Hw4PhotoSearchTests.m
@@ -0,0 +1,34 @@
+//
+//  Hw4PhotoSearchTests.m
+//  Hw4PhotoSearchTests
+//
+//  Created by George Chen on 2/2/14.
+//  Copyright (c) 2014 George Chen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Hw4PhotoSearchTests : XCTestCase
+
+@end
+
+@implementation Hw4PhotoSearchTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
